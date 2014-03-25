171a3aabe957fe589a08164f14c099b98aa65439
George.M <menjivar2009@gmail.com>
2014-03-25 14:08:48 -0400

    edited view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,13 +5,27 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		require 'json'
 		@post = Post.new(user_params)
 		@post.save
+	    #w =  JSON.pretty_generate(JSON.parse(@post.json))
+	    #str = render_to_string(:json => w) 
+	    #file = File.open("C:/Sites/parser_app/tmp/text.txt", 'w') 
+	    #file.write(@str)
+	    #file.close
+		#post.json = w
+		#render :json => w
+		#@post.save
 		redirect_to @post
 	end
 
 	def show
 		@post = Post.find(params[:id])
+		@w =  JSON.pretty_generate(JSON.parse(@post.json))
+
+	    #@str = render_to_string(:json => @w) 
+	    @post.json = @w
+	    @post.save
 	end
 
 	private
