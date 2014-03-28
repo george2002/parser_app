28a4fa712849b8e75405598ec5cd60e5fa4fedf5
George.M <menjivar2009@gmail.com>
2014-03-28 09:31:52 -0400

    idk

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,26 +10,28 @@ class PostsController < ApplicationController
 		@post.save
 
 		if params[:to_screen] || params[:to_pdf]
+			ile = File.open("#{Rails.root}/files/test_file.txt",'w+') do |task|
+	         task.write(@post.json)
+	         end   
 			redirect_to @post	
 	    end
-	    
+
 if params[:to_file] 
 
- @post = Post.new(user_params)
-		@post.save
 	    begin
 			clean_up =  JSON.pretty_generate(JSON.parse(@post.json)) 
 			JSON.parse(@post.json)
 			@post.json = clean_up
 	    	@post.save
+
 	    rescue JSON::ParserError => e
 	   		@post.json = "Invalid Json"
 	   		@post.save
         end	      
        
-	         file = File.open("#{Rails.root}/files/test_file.txt",'w+') do |task|
-	         task.write(@post.json)
-	         end    	
+	        # file = File.open("#{Rails.root}/files/test_file.txt",'w+') do |task|
+	         #task.write(@post.json)
+	         #end    	
              send_file("#{Rails.root}/files/test_file.txt",
               filename: "parsed_json.txt",
               type: "application/txt")  	
