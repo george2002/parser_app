248cf6f886a73e7c1d4d6a76ce8a5235b369fcab
George <menjivar2009@gmail.com>
2014-04-14 15:37:07 -0400

    playing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,9 +29,8 @@ class PostsController < ApplicationController
         end	    
 
 
-            file = File.open("#{Rails.root}/files/test_file.txt",'w+') do |task|
-	         task.write("the end 
-	         	yes")
+            file = File.open("#{Rails.root}/files/test_file.txt",'w') do |task|
+	         task.write("the end \n yes")
 	         end  
 
              send_file("#{Rails.root}/files/test_file.txt",
